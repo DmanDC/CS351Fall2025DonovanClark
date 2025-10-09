9bc73d638aacd06ff024abca6435a427dc95afdb
DmanDC <donovandch1@gmail.com>
2025-10-08 23:18:00 -0500

    titlemapPlatformerFinish

--- CS351Prototype1/Assets/Scripts/ScoreTriggerZone.cs
+++ CS351Prototype1/Assets/Scripts/ScoreTriggerZone.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class ScoreTriggerZone : MonoBehaviour
 {
+    //set this referecne to the jump sound in the the inspector
+    public AudioClip scoreSound;
+
+    //an audio source to play sound effects
+    private AudioSource playerAudio;
+
     //a bool variable to keep tract of if the trigger zone is 
     bool active = true;
-    private void OnTriggerEnter2D (Collider2D collision)
+
+    private void Start()
+    {
+        //Set reference to player audio source
+        playerAudio = GetComponent<AudioSource>();
+
+    }
+
+        private void OnTriggerEnter2D (Collider2D collision)
     {
         //if the trigger zone is active...
         if (active && collision.gameObject.tag == "Player")
@@ -16,8 +30,15 @@ public class ScoreTriggerZone : MonoBehaviour
             //Add 1 to the score when the plaer enters the trigger
             ScoreManager.score++;
 
+            //Play score sound effect
+            playerAudio.PlayOneShot(scoreSound, 1.0f);
+
+            //make it diapper
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.enabled = false;
+
             //Destory this GameObject;
-            Destroy(gameObject);
+            Destroy(gameObject, 2.0f);
         }
         
 
