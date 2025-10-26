a8d54c697d2f5e754cc743588a03ec67caf260dc
DmanDC <donovandch1@gmail.com>
2025-10-26 18:52:14 -0500

    PlatformerwithAni

--- CS351Prototype1/Assets/Scripts/CamFollowPlayer.cs
+++ CS351Prototype1/Assets/Scripts/CamFollowPlayer.cs
@@ -6,7 +6,7 @@ public class CamFollowPlayer : MonoBehaviour
 {
     //Set this reference in the inspector
     public GameObject player;
-
+    public float lookAhead = 2f;
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +16,9 @@ public class CamFollowPlayer : MonoBehaviour
             player.transform.position.x,
             player.transform.position.y,
             transform.position.z);
+
+        float targetX = player.transform.position.x;
+
     }
 
 }
