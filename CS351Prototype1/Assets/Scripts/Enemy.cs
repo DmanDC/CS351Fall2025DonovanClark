030eb3bcfd9341c9068727c3d0d82dc4687fd57e
DmanDC <donovandch1@gmail.com>
2025-11-10 14:20:24 -0600

    FollowAlongLesson8Finished

--- CS351Prototype1/Assets/Scripts/Enemy.cs
+++ CS351Prototype1/Assets/Scripts/Enemy.cs
@@ -10,10 +10,30 @@ public class Enemy : MonoBehaviour
     //a prefab to spawn when the enemy dies
     public GameObject deathEffect;
 
+    //A reference to the health bar
+    private DisplayBar healthBar;
+
+    private void Start()
+    {
+        //Find the HealthBar in the children of the Enemy;
+        healthBar = GetComponentInChildren<DisplayBar>();
+
+        if (healthBar == null )
+        {
+            Debug.LogError("HealthBar (DisplayBar script) not found");
+            return;
+        }
+
+        healthBar.SetMaxValue(health);
+    }
+
 
     public void TakeDamge(int damage) {
         // Subtract the damage dealt from the health
         health -= damage;
+
+        //Update healthbar
+        healthBar.SetValue(health);
 
         //if Health is less than or equal to 0
         if(health <=0)
